39a1f7cce1b20752932bcbd321a726ab6b1b5187
Jaya Devi <cherryjaya143@gmail.com>
2024-07-24 23:47:58 -0700

    Add Files

---
+++ src/ex_07072024/Lab008.java
@@ -0,0 +1,11 @@
+package ex_07072024;
+
+public class Lab008 {
+    public static void main(String[] args) {
+
+        //Keywords & Identifiers
+        //Public,class,package ......
+        //valid identifiers - Hello, Int, getClassName, TOTAL_FEE
+        //invalid identifiers - 1stClass, true, student number, student-email, int
+    }
+}
