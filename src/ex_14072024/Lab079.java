ca59a6a597474df57e3f3a55c5df84aaaad7884f
Jaya Devi <cherryjaya143@gmail.com>
2024-09-17 10:32:08 +0530

    Add All Files

---
+++ src/ex_14072024/Lab079.java
@@ -0,0 +1,14 @@
+package ex_14072024;
+
+public class Lab079 {
+    public static void main(String[] args) {
+        //Ternary Operator
+
+        int result = 10>40 ? 89 : 92;
+        System.out.println(result);
+        String str = 10>20 ? "10" : "twenty";
+        System.out.println(str);
+    }
+
+
+}
